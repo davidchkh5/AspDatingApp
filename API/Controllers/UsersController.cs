dffc130fc30053a6ec6b3279c3bc31a826df7d4c
davidchkh5 <daviti.chkhaidze5@gmail.com>
2023-10-22 14:17:42 +0400

    sect4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,23 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context){
 
         _context = context;
     }
-
+[AllowAnonymous]
 [HttpGet]
 public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
     var users = await _context.Users.ToListAsync();
     return users;
 }
+
 
 [HttpGet("{id}")]
 public async  Task<ActionResult<AppUser>> GetUsers(int id) {
--- API/Program.cs
+++ API/Program.cs
@@ -1,21 +1,19 @@
-using API;
-using Microsoft.EntityFrameworkCore;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddApplicationServices(builder.Configuration);
 
-            opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
-builder.Services.AddCors();
-
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
@@ -25,6 +23,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
