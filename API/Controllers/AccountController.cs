794c668798d2f4e3f4389bc5b1cc388ab7981b81
davidchkh5 <daviti.chkhaidze5@gmail.com>
2023-12-24 16:50:16 +0400

    Section12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using System.Security.Cryptography;
+﻿using System.Security.AccessControl;
+using System.Security.Cryptography;
 using System.Text;
 using API.DTOs;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,10 +13,12 @@ public class AccountController : BaseApiController
 {   
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
-    public AccountController(DataContext context, ITokenService tokenService)
+    private readonly IMapper _mapper;
+    public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
     {
             _tokenService = tokenService;
             _context = context;
+            _mapper = mapper;
             
         
     } 
@@ -26,13 +30,14 @@ public class AccountController : BaseApiController
 
             if(await UserExists(registerDto.UserName)) return BadRequest("User is already taken");
 
+            var user = _mapper.Map<AppUser>(registerDto);
             using var hmac = new HMACSHA512();
 
-           var user = new AppUser {
-            UserName = registerDto.UserName.ToLower(),
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
-           };
+    
+            user.UserName = registerDto.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+            user.PasswordSalt = hmac.Key;
+
            _context.Users.Add(user);
 
            await _context.SaveChangesAsync();
@@ -40,7 +45,8 @@ public class AccountController : BaseApiController
            return new UserDto 
            {
             Username = user.UserName,
-            Token = _tokenService.CreateToken(user)
+            Token = _tokenService.CreateToken(user),
+            KnownAs = user.KnownAs
            };
     }
 
@@ -63,7 +69,8 @@ public class AccountController : BaseApiController
            {
             Username = user.UserName,
             Token = _tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            KnownAs = user.KnownAs
            };   
 
     }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -11,11 +11,12 @@ namespace API.Helpers
         {
                 CreateMap<AppUser, MemberDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
-                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()))
-                .ForMember(dest => dest.Photos, opt => opt.MapFrom(src => src.Photos.Select(p => new PhotoDto { Url = p.Url, IsMain = p.IsMain, Id = p.Id })));
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+                // .ForMember(dest => dest.Photos, opt => opt.MapFrom(src => src.Photos.Select(p => new PhotoDto { Url = p.Url, IsMain = p.IsMain, Id = p.Id })))
 
             CreateMap<Photo, PhotoDto>();    
             CreateMap<MemberUpdateDto,AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
